1af0a92549ac0fdef121f765d46f49f854d77660
Christian Gerhardt <christian.gerhardt3@t-mobile.com>
2020-11-04 15:14:31 -0500

    one more time

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files form the dist directory  
 app.use(express.static(__dirname + '/dist/simple-adding'));
 
 app.get('/*', function(req,res) {
